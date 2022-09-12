e8ad0b3dee892714e244171c6a23e4b749be5e6b
choewy <choewy32@gmail.com>
2022-09-12 11:19:15 +0900

    features route path constants

---
+++ src/constants/route-path.ts
@@ -0,0 +1,5 @@
+export const HOME_PAGE_PATH = '/';
+export const LOGIN_PAGE_PATH = '/login';
+export const LOGOUT_PAGE_PATH = '/logout';
+export const LOG_PAGE_PATH = '/logs';
+export const MY_PAGE_PATH = '/mypage';
