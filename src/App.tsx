a880a161ab7d546e7bd564b24a2118bfb71cbe7c
choewy <choewy32@gmail.com>
2022-09-12 11:22:35 +0900

    features index & App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import '@/App.css';
 
-function App() {
+import { FC, Fragment } from 'react';
+import { Footer } from '@/components/common';
+import { useInitializer } from './hooks';
+import { GNB } from './components/common/gnb';
+import { AppRoutes } from './components/routes';
+
+const App: FC = () => {
+  useInitializer();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <GNB />
+      <AppRoutes />
+      <Footer />
+    </Fragment>
   );
-}
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,23 @@
+import '@/index.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { RecoilRoot } from 'recoil';
+import App from '@/App';
+import reportWebVitals from '@/reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <RecoilRoot>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </RecoilRoot>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
